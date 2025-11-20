1aba1075367fd497503b8b4b976c19ed4c95e600
MoserMichael <mosermichael@gmail.com>
2025-11-20 07:23:47 +0200

    path changes

--- git_html_listing.sh
+++ git_html_listing.sh
@@ -5,6 +5,7 @@
 out_dir="temp_dir"
 git_dir="."
 
+
 out_dir=$(realpath "${out_dir}")
 git_dir=$(realpath "${git_dir}")
 
@@ -13,7 +14,7 @@ script_dir=$(realpath "${script_dir}")
 html_index=""
 
 origin_url=$(git config --get remote.origin.url)
-html_prefix="<html><body><h2>History listing for git repository ${origin_url}</h2><ul>"
+html_prefix="<html><body><h2>History listing for git repository: ${origin_url}</h2><ul>"
 
 #if [[ -d "$out_dir" ]]; then
 #   rm -rf "$out_dir"
@@ -30,9 +31,11 @@ for f in $(git ls-files); do
     dname=$(dirname "$out_file")
     mkdir -p "$dname"
 
-    link_name=${f%.*}
+    link_name=${f}
     link_path="${f}.html"
-    out_html_path="${out_file%.*}.html"
+    out_html_path="${out_file}.html"
+
+    echo "<html><body><h2>History listing for file: ${f}</h2>" >${out_html_path}
 
     git log --color=always --follow --pretty=fuller -p --ignore-cr-at-eol --raw ${f} | ${script_dir}/ansi2html.sh >>${out_html_path} 
 
