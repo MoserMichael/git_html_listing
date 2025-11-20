0e6c62ee691b35961cd9b3a9e72af5f4ea496310
MoserMichael <mosermichael@gmail.com>
2025-11-20 07:17:15 +0200

    zeroth version

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 A shell script for producing a nice html listing for the history of each file.
 The script creates a index.html file that contains a link per html file in given git repository.
 Each linked html file contains the change log for one file contained in the repository.
+
+
+## installation
+
+Copy the files
+- [git_html_listing.sh]() and [ansi2html.sh]() into the same directory
+- you also need the presence of git, sed, gawk
+
+## Thanks
+
+This script uses [ansi2html.sh](https://www.pixelbeat.org/scripts/ansi2html.shq)
---
+++ git_html_listing.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# Licence: LGPLv2
+
+out_dir="temp_dir"
+git_dir="."
+
+out_dir=$(realpath "${out_dir}")
+git_dir=$(realpath "${git_dir}")
+
+script_dir=$(dirname $0)
+script_dir=$(realpath "${script_dir}")
+html_index=""
+
+origin_url=$(git config --get remote.origin.url)
+html_prefix="<html><body><h2>History listing for git repository ${origin_url}</h2><ul>"
+
+#if [[ -d "$out_dir" ]]; then
+#   rm -rf "$out_dir"
+#fi
+
+mkdir -p "$out_dir"
+
+pushd "$git_dir"
+
+for f in $(git ls-files); do
+    echo "Processing $f"
+    out_file="$out_dir/$f"
+    
+    dname=$(dirname "$out_file")
+    mkdir -p "$dname"
+
+    link_name=${f%.*}
+    link_path="${f}.html"
+    out_html_path="${out_file%.*}.html"
+
+    git log --color=always --follow --pretty=fuller -p --ignore-cr-at-eol --raw ${f} | ${script_dir}/ansi2html.sh >>${out_html_path} 
+
+    html_index="${html_index}<li><a href=\"${link_path}\">$f</a>"
+
+done
+
+echo "${html_prefix}${html_index}" >${out_dir}/index.html
+
+
+
